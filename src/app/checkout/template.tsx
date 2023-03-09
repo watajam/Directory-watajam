c7416f4e55762b27977a1f13b42ad4a5fe8bc4b9
watajam <wata124coron@icloud.com>
2023-03-09 22:39:14 +0900

    add

---
+++ src/app/checkout/template.tsx
@@ -0,0 +1,7 @@
+// Layoutファイルだと再レンダリングが聞かないのでtempalte.tsxを使い再レンダリング死体処理を記載する 例：アニメーション
+
+//layoutとtemplateが含まれる場合🔻のように出力される
+  /* <Layout>
+  <Template key={routeParam}>{children}</Template>
+  </Layout> */
+
